7492e3ad29fd71c43908d678fbbfc0cf6084f2b0
Marcus20 <marcusquarles20@gmail.com>
2022-02-21 16:06:13 -0500

    configured typescript support for ts migration

--- components/RideOptionsCard.tsx
+++ components/RideOptionsCard.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-const RideOptionsCard = () => {
+const RideOptionsCard = (): JSX.Element => {
   return (
     <View>
       <Text>Pick a ride</Text>
